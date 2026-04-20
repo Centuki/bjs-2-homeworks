5fd534772d6f74e036f9da97357ac7953ec31a09
Ruslan Somov <rusl.somow@gmail.com>
2026-04-20 08:01:54 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const result = users
+        .filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, age, index, arr) => {
+            acc.sum += age;
+            acc.count = arr.length;
+            return acc;
+        }, {
+            sum: 0,
+            count: 0
+        });
+
+    return result.count > 0 ? result.sum / result.count : 0;
 }
